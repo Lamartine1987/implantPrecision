0e851e7212eb3642ebec0502d53237aa5386653e
LAMARTINE CEZAR MONTEIRO GOMES <lamartine.gomes@recife.pe.gov.br>
2025-06-08 15:27:19 +0000

    tire do subtítulo welcome back e deixe apenas Welcome

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "@/components/auth/LoginForm";
 
 export default function LoginPage() {
   return (
-    <AuthLayout title="Welcome back! Sign in to your account.">
+    <AuthLayout title="Welcome! Sign in to your account.">
       <LoginForm />
     </AuthLayout>
   );
